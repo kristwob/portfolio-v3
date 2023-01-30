ccfa5bcfc3259566f6428269494032c54d2effc7
Kristian Wobbes <kristian.wobbes@gmail.com>
2023-01-30 17:55:57 +0100

    removed header component

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,3 @@
-import Header from "../components/Header";
 import "./global.css";
 
 export default function RootLayout({ children }) {
@@ -10,7 +9,6 @@ export default function RootLayout({ children }) {
       */}
       <head />
       <body>
-        <Header />
         <main>{children}</main>
       </body>
     </html>
