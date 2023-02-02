4962b7f49ce2b35dfe5eccef56febbf06226be9c
Kristian Wobbes <kristian.wobbes@gmail.com>
2023-02-02 21:31:49 +0100

    added sanity head and loading component

---
+++ src/app/studio/[[...index]]/head.jsx
@@ -0,0 +1,12 @@
+export { NextStudioHead } from "next-sanity/studio/head";
+
+import { NextStudioHead } from "next-sanity/studio/head";
+
+export default function CustomStudioHead() {
+  return (
+    <>
+      <NextStudioHead favicons={false} />
+      <link rel="icon" href="https://www.sanity.io/static/images/favicons/favicon-32x32.png" sizes="32x32" type="img/png" />
+    </>
+  );
+}
