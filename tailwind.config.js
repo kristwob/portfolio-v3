d40641866e99b3905d34572a65c37c8e032ea308
Kristian Wobbes <kristian.wobbes@gmail.com>
2023-02-02 21:26:57 +0100

    added orange colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -13,7 +12,9 @@ module.exports = {
   ],
   theme: {
     colors: {
-      orange: "#FF8500",
+      orange_300: "#FF9E00",
+      orange_400: "#FF8500",
+      orange_500: "#FF6D00",
       purple: "#240046",
       white: "#F8F9FA",
       black: "#111111",
