6307209866bd3b0193dd96042c75176dc100f496
Kristian Wobbes <kristian.wobbes@gmail.com>
2023-02-16 14:01:46 +0100

    Footer positioned at bottom

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   const today = new Date();
   const year = today.getFullYear();
   return (
-    <footer className="text-center font-mono">
+    <footer className="text-center font-mono absolute bottom-1 left-0 right-0">
       <p>© {year} Kristian Wobbes</p>
     </footer>
   );
