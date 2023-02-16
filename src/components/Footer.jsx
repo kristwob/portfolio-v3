b4063dc7ea966ca25934a3e83315d33d7457c4d3
Kristian Wobbes <kristian.wobbes@gmail.com>
2023-02-16 15:19:12 +0100

    margin to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   const today = new Date();
   const year = today.getFullYear();
   return (
-    <footer className="text-center font-mono absolute bottom-1 left-0 right-0">
+    <footer className="text-center font-mono mt-10">
       <p>© {year} Kristian Wobbes</p>
     </footer>
   );
