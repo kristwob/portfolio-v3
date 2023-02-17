98ef65ac8d65e4ae232025aa6e09749d26fb07bb
Kristian Wobbes <kristian.wobbes@gmail.com>
2023-02-17 21:35:23 +0100

    sanity images

--- src/lib/sanity.queries.js
+++ src/lib/sanity.queries.js
@@ -1,8 +1,20 @@
 import { groq } from "next-sanity";
 
 export const projectQuery = groq`
-*[_type == "project"]
+*[_type == "project"] 
+{
+    title,
+    "imageUrl": coverImage.asset->{..., metaData},
+    "text": pt::text(content),
+    skills,
+    "slug": slug.current
+}
 `;
+
+export const projectWithSlugQuery = groq`
+    *[_type == "project" && slug.current == $slug][0]
+    `
+
 
 export const cvQuery = groq`
 *[_type == "cv"][0]
