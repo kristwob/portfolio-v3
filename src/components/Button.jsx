0ee965af683cf36a59bc89f68350d3167c11c34b
Kristian Wobbes <kristian.wobbes@gmail.com>
2023-02-17 18:49:21 +0100

    added Experience section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,14 @@
-const Button = ({ title, type, variant }) => {
+const Button = ({ title, type, variant, onClick }) => {
   switch (variant) {
     case "primary":
       return (
-        <button type={type} className="bg-orange_400 hover:bg-orange_300 active:bg-orange_500 transition px-5 py-2 text-black">
+        <button onClick={onClick} type={type} className="bg-orange_400 hover:bg-orange_300 active:bg-orange_500 transition px-5 py-2 text-black">
           {title}
         </button>
       );
     case "secondary":
       return (
-        <button type={type} className="border border-orange_400 hover:border-orange_300 active:border-orange_500 text-orange_400 hover:text-orange_300 active:text-orange_500 px-5 py-2 ">
+        <button onClick={onClick} type={type} className="border border-orange_400 hover:border-orange_300 active:border-orange_500 text-orange_400 hover:text-orange_300 active:text-orange_500 px-5 py-2 ">
           {title}
         </button>
       );
---
+++ src/components/Experience.jsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import Button from "./Button";
+
+const Experience = ({ cv }) => {
+  return (
+    <section id="experience" className="md:mt-[8em]">
+      <h2 className="text-center mb-20">experience</h2>
+      <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+        {cv.map((item) => (
+          <div key={item._key}>
+            <h3>{item.heading}</h3>
+            <p className="text-xl">@ {item.location}</p>
+            <p className="mt-2">{item.description}</p>
+          </div>
+        ))}
+        <Link href="/cv" className="sm:col-start-1 md:col-start-2 md:place-self-center mt-10">
+          <Button title="resume" variant="primary" />
+        </Link>
+      </div>
+    </section>
+  );
+};
+
+export default Experience;
