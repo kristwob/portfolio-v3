f1b134b611c9aeeac7ba2cbb68efc4d8e8cf59ee
Kristian Wobbes <kristian.wobbes@gmail.com>
2023-04-06 14:25:07 +0200

    updated error page

--- src/app/(user)/error.tsx
+++ src/app/(user)/error.tsx
@@ -1,20 +1,25 @@
-'use client'
+'use client' // Error components must be Client components
 
+import Heading from '@/ui/Heading'
 import { useEffect } from 'react'
-import Button from '../../components/ui/Button'
 
-const Error = ({ error, reset }: { error: Error; reset: void }) => {
+export default function Error({ error, reset }: { error: Error; reset: () => void }) {
   useEffect(() => {
+    // Log the error to an error reporting service
     console.error(error)
   }, [error])
+
   return (
     <div>
-      <h1>Something went wrong!</h1>
-      <Button variant='default' onClick={() => reset}>
-        Reset
-      </Button>
+      <Heading>Something went wrong!</Heading>
+      <button
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again
+      </button>
     </div>
   )
 }
-
-export default Error
