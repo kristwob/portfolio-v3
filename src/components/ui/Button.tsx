dee59b49659db9d46c6fd3e6610fb79c54cfe3b5
Kristian Wobbes <kristian.wobbes@gmail.com>
2023-05-22 11:57:41 +0200

    font fixes

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -9,7 +9,7 @@ export const buttonVariants = cva('inline-flex font-mono items-center text-base 
       default: 'bg-slate-900 text-slate-50 hover:bg-slate-800 dark:text-black dark:bg-orange_400 dark:hover:bg-orange_300 dark:active:bg-orange_500 transition px-5 py-2',
       outline:
         'border border-slate-900 text-slate-900 hover:border-slate-700 active:border-slate-800 dark:border-orange_400 dark:hover:border-orange_300 dark:active:border-orange_500 dark:text-orange_400 dark:hover:text-orange_300 dark:active:text-orange_500 px-5 py-2',
-      subtle: 'bg-purple_300 hover:bg-purple_200',
+      subtle: 'bg-slate-300 hover:bg-slate-200',
       destructive: 'text-white hover:bg-red-600 dark:hover:bg-red-600',
       ghost:
         'bg-transparent hover:bg-slate-100 dark:hover:bg-slate-800 dark:text-slate-400 dark:hover:text-white data-[state=open]:bg-transparent dark:data-[state=open]:bg-transparent',
